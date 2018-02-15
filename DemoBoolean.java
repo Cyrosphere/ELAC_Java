722fa6bd54608962d672a65373c2c67440f63063
cyrosphere77 <cyrosphere77@yahoo.com>
2018-02-15 18:21:14 -0500

    test java submission

---
+++ DemoBoolean.java
@@ -0,0 +1,27 @@
+public class DemoBoolean{
+  
+  public static void main(String[] args){
+    int numofzombies = 3;
+    int num2 = 16;
+      if (numofzombies < 5){
+        //if the expression above is true, run code inside the block
+        System.out.println("you might survive the apocalypse.");
+      }
+      else {
+        System.out.println("you are dead.");
+      }
+    }
+
+
+    // random number generate
+    //
+    Math.random(); //random num between 0 and 1
+
+    //create a class game
+  }
+
+}
+
+
+  
+  
