10f86d02a189eca9130c3dba2f05995fd70b0c37
cyrosphere77 <cyrosphere77@yahoo.com>
2018-02-16 23:00:43 -0500

    template

--- GameDriver.java
+++ GameDriver.java
@@ -1,1 +1,35 @@
-what
+//imports used in this class
+import java.util.Scanner;
+
+//Demo HW1 
+public class GameDriver{
+
+  //start of the application
+  public static void main(String[] args){
+    
+    //variable declarations with default values
+    Scanner input = new Scanner(System.in); //initialize Scanner for user input
+
+    //splash art
+    
+    //story begins
+    //wake up in a doungeon
+    
+    
+    //ask player's name
+    
+    
+    //find an exit. left
+    
+    
+    //choose a path
+    
+    
+    //path 1 - swords
+    //path 2 - armor
+    //path 3 - boss
+    
+    
+    //face the boss
+    //decide your destiny
+    //
